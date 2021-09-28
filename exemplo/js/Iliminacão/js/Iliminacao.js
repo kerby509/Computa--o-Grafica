cb4efb2964a101a380958bc9edef01220a54bb20
kerby <lovincekerby99@gmail.com>
2021-09-28 19:53:01 -0300

    commit

---
+++ exemplo/js/Iliminacão/js/Iliminacao.js
@@ -0,0 +1,15 @@
+var scene;//nosso mundo virtual
+var camera; 
+var render;// responsável por gerar as imagems
+
+var elements = [];
+var velCubo = 0.10;
+
+var parametrosGUI;
+
+var controls;
+
+
+/**
+ * INTEFACE GRÁFICA
+ */
